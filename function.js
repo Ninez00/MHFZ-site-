9ff916c4b31f146d84d6d91516fc49df05458dd3
Ninez00 <97367785+Ninez00@users.noreply.github.com>
2022-01-09 00:29:11 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+  function toggleText() {
+      var text = document.getElementById("demo");
+      if (text.style.display === "none") {
+        text.style.display = "block";
+      }
+    }
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
